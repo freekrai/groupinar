304befbb2019fef3748a3900745ee8bbb4920c62
Roger Stringer <roger@datamcfly.com>
2017-10-06 21:45:30 -0700

    close

--- database/migrations/1507230209201_create_webinar_table.js
+++ database/migrations/1507230209201_create_webinar_table.js
@@ -11,6 +11,10 @@ class WebinarsTableSchema extends Schema {
 	  table.string('title', 254).notNullable()
 	  table.string('slug', 254).notNullable().unique()
 	  table.text('description')
+	  table.string('summary', 254)
+	  table.integer('is_public').unsigned().default(0);
+	  table.integer('has_password').unsigned().default(0);
+	  table.string('password', 60)
 	  table.text('meta')
       table.timestamps()
     })
